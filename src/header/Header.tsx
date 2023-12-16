d496c97e303318fe85c6d2d7aa7a9acb5fc13013
white <abhijithkkpurameri@gmail.com>
2023-12-16 17:26:59 +0530

    Basic set up and Header part completed

---
+++ src/header/Header.tsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import './Header.css'
+const Header = () => {
+    const [refresh,userefesh]=useState(false)
+    const ShowNav=()=>{
+        userefesh(!refresh)
+    }
+  return (
+    <div>
+      <div className="bodyy">
+        <div className="inner-body">
+            <div className="logo">POWER VOLTE</div>
+            <div className={refresh ?"list-items1":"list-items"}>
+                <button type='button' className={refresh ? 'toggle-btn1':'toggle-btn'} onClick={ShowNav}>logo</button>
+                <ul>
+                    <li>Home</li>
+                    <li>About us</li>
+                    <li>Our Services</li>
+                    <li>Contact us</li>
+                </ul>
+            </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Header
